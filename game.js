e8273c805d5f9b5d5dd9b64b80e5e1c927f5207d
anirudhhhh <anirudhagrawal2005@gmail.com>
2025-03-30 23:37:16 +0530

    made the bugs more random

--- game.js
+++ game.js
@@ -1,116 +1,140 @@
 const gameState = {
-	score: 0
+  score: 0,
 };
 
 class StartScene extends Phaser.Scene {
-	constructor() {
-		super({ key: 'StartScene' })
-	}
+  constructor() {
+    super({ key: "StartScene" });
+  }
 
-	create() {
-		this.add.text(95, 250, 'Click to Start!', { fontSize: '30px', fill: '#000000' });
-		this.input.on('pointerdown', () => {
-			this.scene.stop('StarScene')
-			this.scene.start('GameScene')
-		})
-	}
+  create() {
+    this.add.text(95, 250, "Click to Start!", {
+      fontSize: "30px",
+      fill: "#000000",
+    });
+    this.input.on("pointerdown", () => {
+      this.scene.stop("StarScene");
+      this.scene.start("GameScene");
+    });
+  }
 }
 
 class GameScene extends Phaser.Scene {
-	constructor(){
-		super({ key: 'GameScene' })
-	}
+  constructor() {
+    super({ key: "GameScene" });
+  }
 
-	preload() {
-		this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png');
-		this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png');
-		this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
-		this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
-		this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
-	}
+  preload() {
+    this.load.image(
+      "bug1",
+      "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png"
+    );
+    this.load.image(
+      "bug2",
+      "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png"
+    );
+    this.load.image(
+      "bug3",
+      "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png"
+    );
+    this.load.image(
+      "platform",
+      "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png"
+    );
+    this.load.image(
+      "codey",
+      "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png"
+    );
+  }
 
+  create() {
+    gameState.player = this.physics.add.sprite(225, 450, "codey").setScale(0.5);
 
-	create() {
-		gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
+    const platforms = this.physics.add.staticGroup();
 
-		const platforms = this.physics.add.staticGroup();
+    platforms.create(225, 490, "platform").setScale(1, 0.3).refreshBody();
 
-		platforms.create(225, 490, 'platform').setScale(1, .3).refreshBody();
+    gameState.scoreText = this.add.text(195, 485, "Score: 0", {
+      fontSize: "15px",
+      fill: "#000000",
+    });
 
-		gameState.scoreText = this.add.text(195, 485, 'Score: 0', { fontSize: '15px', fill: '#000000' });
+    gameState.player.setCollideWorldBounds(true);
 
-		gameState.player.setCollideWorldBounds(true);
+    this.physics.add.collider(gameState.player, platforms);
 
-		this.physics.add.collider(gameState.player, platforms);
+    gameState.cursors = this.input.keyboard.createCursorKeys();
 
-		gameState.cursors = this.input.keyboard.createCursorKeys();
+    const bugs = this.physics.add.group();
 
-		const bugs = this.physics.add.group();
+    const bugGen = () => {
+      const xCoord = Math.random() * 640;
+      const bug = Math.floor(Math.random() * 3);
+      if (bug == 0) {
+        bugs.create(xCoord, 10, "bug1");
+      } else if (bug == 1) {
+        bugs.create(xCoord, 10, "bug2");
+      } else {
+        bugs.create(xCoord, 10, "bug3");
+      }
+    };
 
-		const bugGen = () => {
-			const xCoord = Math.random() * 640
-			if (xCoord < 150){
-				bugs.create(xCoord, 10, 'bug1')
-			}
-			else if(xCoord > 150 && xCoord < 300){
-				bugs.create(xCoord, 10, 'bug3');
-			}
-			else{
-				bugs.create(xCoord, 10, 'bug2')
-			}
-		}
+    const bugGenLoop = this.time.addEvent({
+      delay: 100,
+      callback: bugGen,
+      callbackScope: this,
+      loop: true,
+    });
 
-		const bugGenLoop = this.time.addEvent({
-			delay: 100,
-			callback: bugGen,
-			callbackScope: this,
-			loop: true,
-		});
+    this.physics.add.collider(bugs, platforms, (bug) => {
+      bug.destroy();
+      gameState.score += 10;
+      gameState.scoreText.setText(`Score: ${gameState.score}`);
+    });
 
-		this.physics.add.collider(bugs, platforms, bug => {
-			bug.destroy();
-			gameState.score += 10;
-			gameState.scoreText.setText(`Score: ${gameState.score}`);
-		})
+    this.physics.add.collider(gameState.player, bugs, () => {
+      bugGenLoop.destroy();
+      this.physics.pause();
+      this.add.text(180, 250, "Game Over", {
+        fontSize: "15px",
+        fill: "#000000",
+      });
+      this.add.text(152, 270, "Click to Restart", {
+        fontSize: "15px",
+        fill: "#000000",
+      });
 
-		this.physics.add.collider(gameState.player, bugs, () => {
-			bugGenLoop.destroy();
-			this.physics.pause();
-			this.add.text(180, 250, 'Game Over', { fontSize: '15px', fill: '#000000' });
-			this.add.text(152, 270, 'Click to Restart', { fontSize: '15px', fill: '#000000' });
+      this.input.on("pointerup", () => {
+        gameState.score = 0;
+        this.scene.restart();
+      });
+    });
+  }
 
-			this.input.on('pointerup', () => {
-				gameState.score = 0;
-				this.scene.restart();
-			});
-		});
-	}
-
-	update() {
-		if (gameState.cursors.left.isDown) {
-			gameState.player.setVelocityX(-160);
-		} else if (gameState.cursors.right.isDown) {
-			gameState.player.setVelocityX(160);
-		} else {
-			gameState.player.setVelocityX(0);
-		}
-	}
+  update() {
+    if (gameState.cursors.left.isDown) {
+      gameState.player.setVelocityX(-160);
+    } else if (gameState.cursors.right.isDown) {
+      gameState.player.setVelocityX(160);
+    } else {
+      gameState.player.setVelocityX(0);
+    }
+  }
 }
 
 const config = {
-	type: Phaser.AUTO,
-	width: 450,
-	height: 500,
-	backgroundColor: "b9eaff",
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-			enableBody: true,
-		}
-	},
-	scene: [StartScene, GameScene]
+  type: Phaser.AUTO,
+  width: 450,
+  height: 500,
+  backgroundColor: "b9eaff",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+      enableBody: true,
+    },
+  },
+  scene: [StartScene, GameScene],
 };
 
 const game = new Phaser.Game(config);
-
